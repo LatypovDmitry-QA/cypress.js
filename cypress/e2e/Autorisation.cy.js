c511da7bf9330b8a4aa9ac9fd5f7e9ebde1940e6
Дмитрий <latypov.dmitry777@yandex.ru>
2023-10-09 19:25:39 +0300

    Автотесты на cypress

---
+++ cypress/e2e/Autorisation.cy.js
@@ -0,0 +1,54 @@
+
+describe('Автотесты на форму логина', function () {
+   it('Проверка на позитивный кейс авторизации', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Авторизация прошла успешно').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+   it('Проверка логики восстановления пароля', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('Dima777@mail.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.contains('Успешно отправили пароль на e-mail').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+   it('Проверка на негативный кейс авторизации', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1777');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+   it('Проверка на негативный кейс авторизации', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@v.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+   it('Проверка на негативный кейс валидации', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Нужно исправить проблему валидации').should('be.visible');
+    })
+   it('Проверка на позитивный кейс авторизации', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Авторизация прошла успешно').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+})
+
+
+
+
